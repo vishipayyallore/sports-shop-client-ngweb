ec318503c5ca51275a243474c37c0401476d5a2c
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-10-30 22:36:25 +0530

    Created Product Component

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { ProductComponent } from './product.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProductComponent
+  ],
+  exports: [
+    ProductComponent
+  ],
   imports: [
     CommonModule
   ]
